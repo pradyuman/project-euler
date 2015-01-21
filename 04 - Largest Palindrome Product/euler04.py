dd36fe0f0fb555d5ac8181883a2bb4187bb8aac8
Scyphi <pradyumanvig@hotmail.com>
2015-01-21 00:51:07 -0500

    Solution to Problem 4

--- 04 - Largest Palindrome Product/euler04.py
+++ 04 - Largest Palindrome Product/euler04.py
@@ -1,29 +1,29 @@
-A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-Find the largest palindrome made from the product of two 3-digit numbers.
-
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#Find the largest palindrome made from the product of two 3-digit numbers.
 
 #Solution
-#4613732
+#906609
 
-MAX_NUMBER = 600851475143
-
-def isprime(prime):
-    prime *= 1.0
-    if prime % 2 == 0 and prime != 2:
+#Check if a number is palindrome or not by constructing it backwards
+def isPalindrome(num):
+    test = num
+    #variable that will hold the number being constructed
+    construct = 0
+    while(num != 0):
+        construct = num % 10 + construct * 10
+        num = num // 10
+    if test == construct:
+        return True
+    else:
         return False
-    for divisor in range(3,int(prime ** 0.5) + 1 , 2):
-        if prime % divisor == 0:
-            return False
-    return True
 
-prime = 1
-#Dividing by lowest prime number and then going up
-while MAX_NUMBER != 1:
-	prime += 1
-	while(not(isprime(prime))):
-		prime += 1
-	while not(MAX_NUMBER % prime):
-		MAX_NUMBER = MAX_NUMBER / prime
+def largestPalindrome():
+    palindrome = 0
+    for i in range(100,1000):
+        for j in range(100,1000):
+            if isPalindrome(i * j):
+                if(i * j > palindrome):
+                    palindrome = i * j
+    print(palindrome)
 
-print(prime)
+largestPalindrome()
