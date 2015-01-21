12cf765243ff80decdb2a6df9c3a424ac1bb5236
Scyphi <pradyumanvig@hotmail.com>
2015-01-21 00:03:57 -0500

    Edited the Solution comment in euler03.py

--- 04 - Largest Palindrome Product/euler04.py
+++ 04 - Largest Palindrome Product/euler04.py
@@ -1,9 +1,10 @@
-#Problem 3 - Largest Prime Factor
-#The prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
 
 #Solution
-#6857
+#4613732
 
 MAX_NUMBER = 600851475143
 
