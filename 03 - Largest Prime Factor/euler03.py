eda77220c5a4d32e547be54637545250c3a9b961
Scyphi <pradyumanvig@hotmail.com>
2015-01-20 23:58:48 -0500

    Solution for Problem 3
    Changed up prime logic in the grand scope

--- 03 - Largest Prime Factor/euler03.py
+++ 03 - Largest Prime Factor/euler03.py
@@ -7,28 +7,22 @@
 
 MAX_NUMBER = 600851475143
 
-#Finding next lowest prime number
-def nextprime(num,prime):
-	#Making num a float if num isn't already
-	num *= 1.0
-	#Taking out all even numbers
-	if num == 2 and num > prime:
-		return 2
-	if  num % 2 == 0:
-		return 0
-	for i in range(2,int(num**0.5)+1):
-		if num % i == 0:
-			return 0
-		elif i > prime:
-			return i
+def isprime(prime):
+    prime *= 1.0
+    if prime % 2 == 0 and prime != 2:
+        return False
+    for divisor in range(3,int(prime ** 0.5) + 1 , 2):
+        if prime % divisor == 0:
+            return False
+    return True
 
 prime = 1
 #Dividing by lowest prime number and then going up
 while MAX_NUMBER != 1:
-	for i in range(2,int(MAX_NUMBER ** 0.5)):
-		prime = nextprime(i, prime)
-		break
-	while not(MAX_NUMBER % 5):
+	prime += 1
+	while(not(isprime(prime))):
+		prime += 1
+	while not(MAX_NUMBER % prime):
 		MAX_NUMBER = MAX_NUMBER / prime
 
 print(prime)
